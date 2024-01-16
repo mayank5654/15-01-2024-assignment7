ab9176c96dd114ffde6686872c50d90d9e88e19c
mayank5654 <mayankkaushal99@gmail.com>
2024-01-16 12:44:48 +0530

    intial commit

---
+++ assignment7/Controllers/PlayersController.cs
@@ -0,0 +1,36 @@
+﻿using assignment7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assignment7.Controllers
+{
+    public class PlayersController : Controller
+    {
+        static List<Player> players = new List<Player>()
+        {
+            new Player {Pid=1,PName="ms dhoni",PCountry="india",Ptype="wicket keeper"},
+            new Player {Pid=2,PName="bumrah",PCountry="india",Ptype="bowler"},
+            new Player {Pid=3,PName="rohit sharma",PCountry="india",Ptype="batsman"}
+        };
+
+        public IActionResult Index()
+        {
+            return View(players);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Player());
+        }
+        [HttpPost]
+        public IActionResult Create
+            (Player pro)
+        {
+            if (ModelState.IsValid)
+            {
+                players.Add(pro);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ assignment7/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace assignment7.Models
+{
+    public class Player
+    {
+        public int Pid { get; set; }
+        public string PName { get; set; }
+        public string PCountry { get; set; }
+        public string Ptype {  get; set; }
+
+    }
+}
